e2ba9ffec44105a1c56d96f333d8c404d1a92572
Sebastián Personal <sebastian.ariel.cardozo@gmail.com>
2025-05-16 10:57:12 -0300

    test(deploy): deploy google

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 // src/config/configuration.ts
 export default () => ({
-  port: parseInt(process.env.PORT ?? '3000', 10),
+  port: parseInt(process.env.PORT ?? '8080', 10),
   database: {
     host: process.env.DATABASE_HOST ?? 'localhost',
     port: parseInt(process.env.DATABASE_PORT ?? '5432', 10),
